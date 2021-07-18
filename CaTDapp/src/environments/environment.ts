bb574ae824504606324a8fcade7b9e0a28f344b2
Carlosanamurov <carlosanamuroventura@upeu.edu.pe>
2021-07-18 18:23:15 -0500

    Examen 3 success

---
+++ CaTDapp/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDVT2LHQU1yYxoYAlbCyWtdeICsdCUazGM",
+    authDomain: "examen--3.firebaseapp.com",
+    projectId: "examen--3",
+    storageBucket: "examen--3.appspot.com",
+    messagingSenderId: "337958723285",
+    appId: "1:337958723285:web:8a2fcfefb07459859bcc5c",
+    measurementId: "G-WT0YCH7B6S"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
